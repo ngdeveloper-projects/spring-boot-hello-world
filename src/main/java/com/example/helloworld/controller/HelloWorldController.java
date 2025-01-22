d906fd8644905af57f3de08c48f0d0df4fd06315
NgDeveloper <52323005+ngdeveloper-projects@users.noreply.github.com>
2025-01-22 17:25:00 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello1")
+    @GetMapping("/hello")
     public String sendGreetings() {
         return "Hello, World!";
     }
